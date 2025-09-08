7082c37861ba96b31112f42136de6e64339269a0
fraven01 <74104856+fraven01@users.noreply.github.com>
2025-09-08 16:20:02 +0200

    Pin python-json-logger in requirements

--- noesis2/settings/production.py
+++ noesis2/settings/production.py
@@ -5,4 +5,9 @@ DEBUG = False
 
 # Use JSON logging in production
 LOGGING['root']['handlers'] = ['json_console']
+LOGGING['loggers']['django.request'] = {
+    'handlers': ['mail_admins', 'json_console'],
+    'level': 'ERROR',
+    'propagate': False,
+}
 
