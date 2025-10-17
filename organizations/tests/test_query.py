d77808e36961c6a0441bb2ae64888e8e11ded33a
fraven01 <74104856+fraven01@users.noreply.github.com>
2025-10-17 11:49:01 +0200

    Refine multi-tenant query tests

--- organizations/tests/test_query.py
+++ organizations/tests/test_query.py
@@ -3,80 +3,64 @@ import pytest
 from organizations.tests.factories import OrganizationFactory
 from projects.tests.factories import ProjectFactory
 from workflows.tests.factories import WorkflowInstanceFactory
-from documents.tests.factories import DocumentFactory
 from organizations.utils import set_current_organization
 from projects.models import Project
 from workflows.models import WorkflowInstance
-from documents.models import Document
+
+
+@pytest.fixture
+def project_factory():
+    return ProjectFactory
+
+
+@pytest.fixture
+def workflow_instance_factory():
+    return WorkflowInstanceFactory
 
 
 @pytest.mark.django_db
-def test_project_missing_org_returns_empty():
+def test_project_missing_org_returns_empty(project_factory):
     org = OrganizationFactory()
-    ProjectFactory(organization=org)
+    project_factory(organization=org)
     assert list(Project.objects.all()) == []
 
 
 @pytest.mark.django_db
-def test_project_wrong_org_returns_empty():
+def test_project_wrong_org_returns_empty(project_factory):
     org1 = OrganizationFactory()
     org2 = OrganizationFactory()
-    ProjectFactory(organization=org1)
+    project_factory(organization=org1)
     with set_current_organization(org2):
         assert list(Project.objects.all()) == []
 
 
 @pytest.mark.django_db
-def test_project_correct_org_returns_result():
+def test_project_correct_org_returns_result(project_factory):
     org = OrganizationFactory()
-    project = ProjectFactory(organization=org)
+    project = project_factory(organization=org)
     with set_current_organization(org):
         assert list(Project.objects.all()) == [project]
 
 
 @pytest.mark.django_db
-def test_document_missing_org_returns_empty():
+def test_workflowinstance_missing_org_returns_empty(workflow_instance_factory):
     org = OrganizationFactory()
-    DocumentFactory(project__organization=org)
-    assert list(Document.objects.all()) == []
-
-
-@pytest.mark.django_db
-def test_document_wrong_org_returns_empty():
-    org1 = OrganizationFactory()
-    org2 = OrganizationFactory()
-    DocumentFactory(project__organization=org1)
-    with set_current_organization(org2):
-        assert list(Document.objects.all()) == []
-
-
-@pytest.mark.django_db
-def test_document_correct_org_returns_result():
-    org = OrganizationFactory()
-    document = DocumentFactory(project__organization=org)
-    with set_current_organization(org):
-        assert list(Document.objects.all()) == [document]
-
-
-@pytest.mark.django_db
-def test_workflowinstance_missing_org_returns_empty():
-    org = OrganizationFactory()
-    WorkflowInstanceFactory(project__organization=org)
+    workflow_instance_factory(project__organization=org)
     assert list(WorkflowInstance.objects.all()) == []
 
 
 @pytest.mark.django_db
-def test_workflowinstance_wrong_org_returns_empty():
+def test_workflowinstance_wrong_org_returns_empty(workflow_instance_factory):
     org1 = OrganizationFactory()
     org2 = OrganizationFactory()
-    WorkflowInstanceFactory(project__organization=org1)
+    workflow_instance_factory(project__organization=org1)
     with set_current_organization(org2):
         assert list(WorkflowInstance.objects.all()) == []
 
 
 @pytest.mark.django_db
-def test_workflowinstance_correct_org_returns_result():
+def test_workflowinstance_correct_org_returns_result(workflow_instance_factory):
     org = OrganizationFactory()
-    instance = WorkflowInstanceFactory(project__organization=org)
+    instance = workflow_instance_factory(project__organization=org)
     with set_current_organization(org):
         assert list(WorkflowInstance.objects.all()) == [instance]
