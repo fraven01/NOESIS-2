7026ceffce4c8d87fdbfead4c916c05d0d671a53
fraven01 <74104856+fraven01@users.noreply.github.com>
2025-10-17 09:56:33 +0200

    Remove document relationship test dependency

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,17 +1,9 @@
 import pytest
 
 from .factories import UserFactory
-from documents.tests.factories import DocumentFactory
 
 
 @pytest.mark.django_db
 def test_user_creation():
     user = UserFactory(email="test@example.com")
     assert user.email == "test@example.com"
-
-
-@pytest.mark.django_db
-def test_user_document_relationship():
-    user = UserFactory()
-    document = DocumentFactory(owner=user)
-    assert document.owner == user
